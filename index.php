3c519034716af46f76c43b11398172806be0c90f
Isabela Gomes Silva <73182650+IsaGSilva@users.noreply.github.com>
2021-10-08 12:27:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
  <button type="button" class="btn btn-link">Aceitar</button>
   <button type="button" class="btn btn-link">Erro</button>
+ <button type="button" class="btn btn-link">Sair</button>
 </html>
